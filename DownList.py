c4a6feb114e51766edee321f41da1cc1adfd8fec
ilyakion <62231992+ilyakion@users.noreply.github.com>
2021-09-18 10:27:56 +0300

    Update DownList.py

--- DownList.py
+++ DownList.py
@@ -1,1 +1,24 @@
-
+def DownList(l):
+    if l=='':
+        return l
+    nl=""
+    n=[l[0],1]
+    for i in l[1:]:
+        if n[0]==i:
+            n[1]+=1
+        else:
+            if n[1]==1:
+                nl+=n[0]
+            else:
+                nl+=str(n[1])
+                nl+=n[0]
+            n[0]=i
+            n[1]=1
+    if n[1]==1:
+        nl+=n[0]
+    else:
+        nl+=str(n[1])
+        nl+=n[0]
+    n[0]=i
+    n[1]=1
+    return nl
